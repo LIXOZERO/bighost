c0fed4197f10e0af247fc75cdb543b87ad18f03f
LIXOZERO <134426331+LIXOZERO@users.noreply.github.com>
2023-05-29 21:21:53 +0200

    Update index.js
    Co-Authored-By: Lucas Fulano <lucasalbertofulano@Gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,20 @@ import { useContext } from 'react'
 import MyContext from '@/context/MyContext'
 import LoadingFullScreen from '@/components/Universal/LoadingFullScreen'
 
-export default function Home() {
-  const { loadingFullScreen } = useContext(MyContext)
+export default function Home ()
+{
+  const { loadingFullScreen } = useContext( MyContext )
   return (
     <>
-      {loadingFullScreen ? (
+      { loadingFullScreen ? (
         <LoadingFullScreen />
       ) : (
         <>
           <Header />
           <HeroMain />
-          <HeroMain />
+
         </>
-      )}
+      ) }
     </>
   )
 }
